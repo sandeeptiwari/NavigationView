4ee2c7b49db959d34cf72329547912ecbe88f95f
Sandeep Tiwari <sandeep2.tiwari@ARICENT.COM>
2016-03-21 19:38:58 +0530

    handle selection

--- app/src/main/java/techiesandeep/com/navigationdrawer/MainActivity.java
+++ app/src/main/java/techiesandeep/com/navigationdrawer/MainActivity.java
@@ -1,12 +1,16 @@
 package techiesandeep.com.navigationdrawer;
 
 import android.app.FragmentTransaction;
+import android.graphics.Color;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
+    private MenuItem mPrevItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,19 @@ public class MainActivity extends AppCompatActivity {
         initUI();
     }
 
+
     private void initUI() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setItemIconTintList(null);
+        mPrevItem = mNavigationView.getMenu().getItem(0);
+        mPrevItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                R.mipmap.home_sel));
+        SpannableString s = new SpannableString(mPrevItem.getTitle());
+        s.setSpan(new ForegroundColorSpan(Color.parseColor("#2196F3")), 0, s.length(), 0);
+        mPrevItem.setTitle(s);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -39,34 +53,84 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
                 //if the selected item is in checked state, if it's not in checked state, make it in checked state
-                if (menuItem.isChecked()) menuItem.setChecked(false);
-                else menuItem.setChecked(true);
+                /*if (menuItem.isChecked()) menuItem.setChecked(false);
+                else*/ menuItem.setChecked(true);
+
 
                 //Closing drawer on item click
                 mDrawerLayout.closeDrawers();
                 FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                if(mPrevItem != null){
+
+
+
+                    switch (mPrevItem.getItemId()) {
+                        case R.id.drawer_home:
+                            mPrevItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                    R.mipmap.home));
+                            SpannableString s1 = new SpannableString(mPrevItem.getTitle());
+                            s1.setSpan(new ForegroundColorSpan(Color.parseColor("#000000")), 0, s1.length(), 0);
+                           // mPrevItem.getActionView().setBackgroundColor(Color.parseColor("#FFFFFF"));
+                            mPrevItem.setTitle(s1);
+                            break;
+                        case R.id.drawer_about:
+                            mPrevItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                R.mipmap.aboutus));
+                            SpannableString s2 = new SpannableString(mPrevItem.getTitle());
+                            s2.setSpan(new ForegroundColorSpan(Color.parseColor("#000000")), 0, s2.length(), 0);
+                           // mPrevItem.getActionView().setBackgroundColor(Color.parseColor("#FFFFFF"));
+                            mPrevItem.setTitle(s2);
+                            break;
+                        case R.id.drawer_settings:
+                            mPrevItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                    R.mipmap.setting));
+                            SpannableString s3 = new SpannableString(mPrevItem.getTitle());
+                            s3.setSpan(new ForegroundColorSpan(Color.parseColor("#000000")), 0, s3.length(), 0);
+                           // mPrevItem.getActionView().setBackgroundColor(Color.parseColor("#FFFFFF"));
+                            mPrevItem.setTitle(s3);
+                            break;
+                    }
+                }
                 switch (menuItem.getItemId()) {
-
                     //Replacing the main content with ContentFragment Which is our Inbox View;
-
-
                     case R.id.drawer_home:
                         Toast.makeText(getApplicationContext(), "Home Selected", Toast.LENGTH_SHORT).show();
                         HomeFragment fragment = new HomeFragment();
                         fragmentTransaction.replace(R.id.container, fragment);
                         fragmentTransaction.commit();
+                        menuItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                R.mipmap.home_sel));
+                        SpannableString s1 = new SpannableString(menuItem.getTitle());
+                        s1.setSpan(new ForegroundColorSpan(Color.parseColor("#2196F3")), 0, s1.length(), 0);
+                        menuItem.setTitle(s1);
+                        //menuItem.getActionView().setBackgroundColor(Color.parseColor("#EEEEEE"));
+                        mPrevItem = menuItem;
                         return true;
                     case R.id.drawer_about:
                         Toast.makeText(getApplicationContext(), "About Selected", Toast.LENGTH_SHORT).show();
                         AboutFragment aboutFragment = new AboutFragment();
                         fragmentTransaction.replace(R.id.container, aboutFragment);
                         fragmentTransaction.commit();
+                        menuItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                R.mipmap.aboutus_sel));
+                        SpannableString s2 = new SpannableString(menuItem.getTitle());
+                        s2.setSpan(new ForegroundColorSpan(Color.parseColor("#2196F3")), 0, s2.length(), 0);
+                        menuItem.setTitle(s2);
+                        //menuItem.getActionView().setBackgroundColor(Color.parseColor("#EEEEEE"));
+                        mPrevItem = menuItem;
                         return true;
                     case R.id.drawer_settings:
                         Toast.makeText(getApplicationContext(), "Setting Selected", Toast.LENGTH_SHORT).show();
                         SettingFragment settingFragment = new SettingFragment();
                         fragmentTransaction.replace(R.id.container, settingFragment);
                         fragmentTransaction.commit();
+                        menuItem.setIcon(ContextCompat.getDrawable(MainActivity.this,
+                                R.mipmap.setting_sel));
+                        SpannableString s3 = new SpannableString(menuItem.getTitle());
+                        s3.setSpan(new ForegroundColorSpan(Color.parseColor("#2196F3")), 0, s3.length(), 0);
+                        menuItem.setTitle(s3);
+                        //menuItem.getActionView().setBackgroundColor(Color.parseColor("#EEEEEE"));
+                        mPrevItem = menuItem;
                         return true;
                     default:
                         Toast.makeText(getApplicationContext(), "Somethings Wrong", Toast.LENGTH_SHORT).show();
